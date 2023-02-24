21eceeed4e1592920f64d89e4feef463013a4fb0
Mustafa Binalhag <mbin828@gmail.com>
2023-02-24 12:24:50 -0500

    onscroll animations with framer motion and tracker hook

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
         <>
 		<div className="Navbar">
 			<div className="Navbar-header">
-                <h1>Tel: 929-123-4555</h1>
+                <h3>Tel: 929-123-4555</h3>
             </div>
 		</div>
         <div className="Navbar-content">
--- client/src/screens/Landing.tsx
+++ client/src/screens/Landing.tsx
@@ -1,21 +1,33 @@
 import { useRef } from "react";
 import tracker from "../util/tracker";
+import { motion } from "framer-motion";
+
+let animationValues = {
+	initial: { y: "-5rem", opacity: 0 },
+	animate: { y: "0rem", opacity: 1 },
+	transition: { duration: 0.5 },
+};
 
 const Landing = () => {
-	const divRef = useRef<HTMLDivElement>(null);
-	const inView = tracker(divRef); // custom hook that returns a boolean when ref is in view
-	if (inView) {
-		console.log("in viewport:", divRef.current);
-	} else {
-		console.log("not in view");
-	}
-
+	const catalogRef = useRef<HTMLDivElement>(null);
+	const featuredRef = useRef<HTMLDivElement>(null);
+	// custom hook that returns a boolean when ref is in view
+	// this obj keeps track of all refs
+	const viewportTrackers = {
+		catalog: tracker(catalogRef),
+		featured: tracker(featuredRef),
+	};
+	
 	return (
-		<>
+		<div className="Landing">
 			<div className="Landing-tempage"></div>
-			<div className="Landing-tempage"></div>
-			<div ref={divRef} className="Landing-tempage"></div>
-		</>
+			<div ref={catalogRef} style={{ backgroundColor: "red" }} className="Landing-tempage">
+				{viewportTrackers.catalog && <motion.div {...animationValues}>BROWSE OUR CATEGORIES</motion.div>}
+			</div>
+			<div ref={featuredRef} style={{ backgroundColor: "blue" }} className="Landing-tempage">
+				{viewportTrackers.featured && <motion.div {...animationValues}>FEATURED PRODUCTS</motion.div>}
+			</div>
+		</div>
 	);
 };
 export default Landing;
